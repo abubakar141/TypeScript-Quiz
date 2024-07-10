0ab9f15aff7d3d77693347777e67595f28c003f5
Abu Bakar <abubakargamerz1415@gmail.com>
2024-07-09 23:15:29 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+"use strict";
+// let notSure: any = 4;
+// notSure = "maybe a string instead";
+// notSure = false;
+// enum Color2 {Red =1, Green = 2, Blue = 4}
+// var c = Color2["Blue"
+//         console.log(colorindex);
+// ]
+// const enum Color1 {Red =1, Green, Blue}
+// var colorName: string = Color[2];
+// console.log(colorName);
+// var myAdd: (baseValue: number, increment: number) =>number=
+// function(x: number, y: number): number { return x+y };
+// var buildName: (firstName: string, lastName?: string)=>string=
+// function(firstName:string, lastName = "Khan"): string {
+//         if(lastName)
+//                 return firstName
+//          + " " + lastName;
+//          else: string
+//          return firstName
+// }
+// var buildNameFun: (...rest:  string[], fname: string )=> string=
+// function (...restOfName: string[],firstName: string) {
+//          return firstName + " " + restOfName.join(" ");
+// }
+// console.log(buildNameFun("Khan", "Zia", "U."));
+// function myCallBack(text: string){
+//         console.log("inside myCallBack"+text);
+// }
+// function addWithUnion(arg:1: string | number | boolean)
+// if (typeof arg1 === "string"){
+//         return arg1 + "Hello"
+// }
+// if (typeof arg1 === "number"){
+//         return arg1 + 10;
+// }
+// if (typeof arg1 === "boolean"){
+//         return arg1 && false;
+// }
